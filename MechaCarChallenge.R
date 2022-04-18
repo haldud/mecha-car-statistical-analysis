e31670ac2781135cf72c7edecda9635b6ae6a92d
m031506 <dudakovic.halil@mayo.edu>
2022-04-17 21:57:12 -0500

    deliverable 3 code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -22,3 +22,16 @@ total_summary <- mecha_coil_data %>% summarize(mean = mean(PSI), median = median
 
 # get the mean, median, variance, and standard deviation for PSI grouped by manufacturing lot
 lot_summary <- mecha_coil_data %>% group_by(Manufacturing_Lot) %>% summarize(mean = mean(PSI), median = median(PSI), variance = var(PSI), sd = sd(PSI))
+
+
+
+# Deliverable 3: T-Tests on Suspension Coils
+
+# determine if the PSI across all manufacturing lots is statistically different
+# from the population mean of 1,500 pounds per square inch
+t.test(mecha_coil_data$PSI, mu = 1500)
+
+# three additional tests with different subsets of manufacturing lots
+t.test(subset(mecha_coil_data, Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
+t.test(subset(mecha_coil_data, Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
+t.test(subset(mecha_coil_data, Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
